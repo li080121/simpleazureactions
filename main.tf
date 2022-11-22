ce151f5e0fec21d12b94fc3f6dbf9e5f97b59cbe
li080121 <112656335+li080121@users.noreply.github.com>
2022-11-21 23:12:30 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,15 @@ terraform {
 provider "azurerm" {
  features {}
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "TerraformDemo"
+    storage_account_name = "terraformcodeops1"
+    container_name       = "tfstatefile"
+    key                  = "simple.terraform.tfstate"
+  }
+}
  
 resource "azurerm_resource_group" "rg" {
  name     = "myTFResourceGroup99"
